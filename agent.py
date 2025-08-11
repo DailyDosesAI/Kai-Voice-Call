0c7aefb6ec586a5b02287704fdafeaddc422c146
Fateme Maqazei <fatemeh.m@dailydoses.ai>
2025-08-11 17:13:21 +0330

    First Commit - Init

---
+++ agent.py
@@ -0,0 +1,180 @@
+import httpx
+import asyncio
+
+from typing import Any, List
+from enum import Enum
+from dotenv import load_dotenv
+from livekit import agents
+from livekit.agents import AgentSession, RoomInputOptions
+from livekit.plugins import noise_cancellation, openai
+from langfuse import Langfuse
+from livekit.agents import ConversationItemAddedEvent
+from pydantic import BaseModel
+from pydantic_settings import BaseSettings
+
+load_dotenv()
+
+
+class KaiSettings(BaseSettings):
+    openai_api_key: str
+
+    livekit_api_key: str
+    livekit_api_secret: str
+    livekit_url: str
+
+    langfuse_public_key: str
+    langfuse_secret_key: str
+    langfuse_host: str
+
+    kai_api_base_url: str
+    kai_api_secret_key: str
+
+    class Config:
+        env_file = ".env"
+
+
+settings = KaiSettings()
+
+langfuse = Langfuse(
+    public_key=settings.langfuse_public_key,
+    secret_key=settings.langfuse_secret_key,
+    host=settings.langfuse_host,
+)
+
+# Kai LLM Agent
+from livekit.agents import Agent
+
+
+class KaiSessionMetadata(BaseModel):
+    voice_call_id: int
+
+
+class KaiSessionParticipant(BaseModel):
+    id: int
+    name: str
+    cefr_level: str
+    native_language: str
+
+
+class RequestAnalyseVoiceCallMessageRole(Enum):
+    student = "student"
+    kai = "kai"
+
+
+class RequestAnalyseVoiceCallMessage(BaseModel):
+    role: RequestAnalyseVoiceCallMessageRole
+    content: str
+
+
+class RequestAnalyseVoiceCall(BaseModel):
+    messages: List[RequestAnalyseVoiceCallMessage]
+
+
+class Kai(Agent):
+    def __init__(self) -> None:
+        prompt = langfuse.get_prompt("kai_base_prompt")
+        super().__init__(
+            instructions=prompt.compile(
+                user_name="Fatem",
+                user_cefr_level="B2",
+                user_native_language="fa",
+            )
+        )
+
+
+class KaiSession(AgentSession):
+    def __init__(self, ctx: agents.JobContext):
+        super().__init__(
+            llm=openai.realtime.RealtimeModel(voice="echo"),
+        )
+        print(ctx.room.name)
+        self.ctx = ctx
+        self.metadata = KaiSessionMetadata(
+            voice_call_id=int(ctx.room.name),
+        )
+        self.messages = RequestAnalyseVoiceCall(messages=[])
+        self.participant = None
+
+    def _load_participant(self):
+        print(self.ctx.room.remote_participants)
+        for _, participant in self.ctx.room.remote_participants.items():
+            self.participant = KaiSessionParticipant.model_validate_json(
+                participant.metadata
+            )
+            break
+
+    async def _analyze_messages(self, messages: RequestAnalyseVoiceCall):
+        if self.participant is None:
+            print("No participant found")
+            return
+
+        print(messages)
+        async with httpx.AsyncClient() as client:
+            response = await client.post(
+                f"{settings.kai_api_base_url}/kai/voice-call/{self.metadata.voice_call_id}/analyze/",
+                json=messages.model_dump(mode="json"),
+                headers={"Authorization": f"ApiKey {settings.kai_api_secret_key}"},
+            )
+            response.raise_for_status()
+
+    async def on_conversation_item_added(self, event: ConversationItemAddedEvent):
+        self._load_participant()
+        if len(self.messages.messages) >= 4:
+            print("Analyzing messages")
+            await self._analyze_messages(self.messages)
+            self.messages.messages = []
+
+        print(self.messages.messages)
+        if event.item.role == "user":
+            self.messages.messages.append(
+                RequestAnalyseVoiceCallMessage(
+                    role=RequestAnalyseVoiceCallMessageRole.student,
+                    content="\n".join(event.item.content),
+                )
+            )
+        elif event.item.role == "assistant":
+            self.messages.messages.append(
+                RequestAnalyseVoiceCallMessage(
+                    role=RequestAnalyseVoiceCallMessageRole.kai,
+                    content="\n".join(event.item.content),
+                )
+            )
+
+    async def on_participant_disconnected(self):
+        await self._analyze_messages(self.messages)
+        self.messages.messages = []
+
+    async def on_participant_connected(self):
+        self._load_participant()
+
+
+# Entrypoint
+async def entrypoint(ctx: agents.JobContext):
+    kai_session = KaiSession(ctx)
+    await kai_session.start(
+        room=ctx.room,
+        agent=Kai(),
+        room_input_options=RoomInputOptions(
+            noise_cancellation=noise_cancellation.BVC(),
+        ),
+    )
+
+    @kai_session.on("conversation_item_added")
+    def on_conversation_item_added(event: ConversationItemAddedEvent):
+        asyncio.create_task(kai_session.on_conversation_item_added(event))
+
+    @ctx.room.on("participant_disconnected")
+    def on_participant_disconnected(event: Any):
+        asyncio.create_task(kai_session.on_participant_disconnected())
+        # Here you could insert into your database
+
+    @ctx.room.on("participant_connected")
+    def on_participant_connected(event: Any):
+        print("Participant connected")
+        asyncio.create_task(kai_session.on_participant_connected())
+
+    await kai_session.generate_reply(instructions="Greet the student in English")
+
+
+if __name__ == "__main__":
+    agents.cli.run_app(agents.WorkerOptions(entrypoint_fnc=entrypoint))
